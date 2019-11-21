8bd6a530e1ae0bd6116891752c1b5619e1812130
ulskHeprit <xumuk495.x@ya.ru>
2019-11-21 04:01:26 +0400

    add controller example

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+    function about()
+    {
+        $tags = ['обучение', 'программирование', 'php', 'oop'];
+        return view('about', ['tags' => $tags]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    $tags = ['обучение', 'программирование', 'php', 'oop'];
-    return view('about', ['tags' => $tags]);
-});
+Route::get('/about', 'PageController@about');
